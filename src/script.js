ff8c44cefed8c1d466c8e219f2ba24de7e73bcde
huskyhoochu <dfg1499@gmail.com>
2018-01-27 16:14:55 +0900

    maps

--- src/script.js
+++ src/script.js
@@ -168,3 +168,26 @@ const emily = new SmithPerson('emily', 1990, 'Grand', 'Japan');
 console.log(john);
 console.log(emily);
 */
+
+// Maps
+/*
+const question = new Map();
+question.set('question', 'What is th official name of the latest major JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+
+question.set(true, 'Correct answer!');
+question.set(false, 'Wrong, Please try again.');
+
+console.log(question.get('question'));
+
+question.delete(4);
+question.delete(4);
+
+console.log(question.entries());
+
+question.forEach((key, value) => (console.log(`${key}: ${value}`)));
+*/
